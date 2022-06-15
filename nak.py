163e3c17fcc66d6590b0cbe2bcb2362467fdfef5
IcyDingo <78955247+IcyDingo@users.noreply.github.com>
2022-06-15 23:12:26 +0200

    added submit button

--- nak.py
+++ nak.py
@@ -9,7 +9,7 @@ st.image("https://shop13443.sfstatic.io/upload_dir/shop/products/55/3155b.jpg")
 with st.form(key="my_form"):
     seasons = st.slider(label= "Antal Seasoner", min_value=1, max_value=12)
     eps = st.slider(label="Antal Episoder",min_value=1, max_value=12 )
-    antal = st.st.slider("Antal Foresalg", min_value = 1, max_value = 5)
+    antal = st.slider("Antal Foresalg", min_value = 1, max_value = 5)
     submit = st.form_submit_button("Submit")
 
 
